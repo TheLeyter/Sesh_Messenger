d82135970cbb2e9d80cbcd4a7f362cc55d93290a
TheLeyter <v.lopatinets@ukr.net>
2021-05-26 00:33:45 +0300

    add refresh token interceptor

--- app/src/main/java/com/example/sesh/service/ApiCoreService.java
+++ app/src/main/java/com/example/sesh/service/ApiCoreService.java
@@ -1,5 +1,6 @@
 package com.example.sesh.service;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,8 +8,12 @@ public class ApiCoreService {
     private static  ApiCoreService mInstance;
     private static String BASE_URL = "http://192.168.1.101:5000/";
     private Retrofit mRetrofit;
+    private OkHttpClient client;
 
     private ApiCoreService(){
+        client = new OkHttpClient.Builder()
+                .addInterceptor()
+
         mRetrofit = new  Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
